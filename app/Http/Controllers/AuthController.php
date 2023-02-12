e0942663339e2fdedb627f0f237627a231ad0913
eapDimitriosCollier <dimitrios.collier@gmail.com>
2023-02-12 12:32:21 +0200

    Remove Repository-Service pattern

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\LoggedInUserResource;
-use App\Services\AuthService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response as HttpResponse;
@@ -12,20 +11,12 @@ use Illuminate\Support\Facades\Response;
 use OpenApi\Attributes as OAT;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @param  AuthService  $authService
-     * @return void
-     */
-    public function __construct(private AuthService $authService)
-    {
-        //
-    }
-
     /**
      * Login a user.
      *
@@ -73,7 +64,11 @@ class AuthController extends Controller
     )]
     public function login(LoginRequest $request): JsonResponse
     {
-        $user = $this->authService->login($request);
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return abort(401, 'Invalid credentials.');
+        }
 
         return Response::json(new LoggedInUserResource($user));
     }
@@ -95,11 +90,24 @@ class AuthController extends Controller
                 response: HttpResponse::HTTP_NO_CONTENT,
                 description: 'No content'
             ),
+            new OAT\Response(
+                response: HttpResponse::HTTP_UNAUTHORIZED,
+                description: 'Unauthorized',
+                content: new OAT\JsonContent(
+                    properties: [
+                        new OAT\Property(
+                            property: 'message',
+                            type: 'string',
+                            example: 'Invalid credentials.'
+                        ),
+                    ]
+                )
+            ),
         ]
     )]
     public function logout(Request $request): JsonResponse
     {
-        $this->authService->logout($request->user());
+        $request->user()->currentAccessToken()->delete();
 
         return Response::json(null, HttpResponse::HTTP_NO_CONTENT);
     }
