4cc8c8da362363bbebc1ca78ab2d562c4188098f
eapDimitriosCollier <dimitrios.collier@gmail.com>
2023-02-17 19:05:41 +0200

    Home icon

--- crust/src/components/AppBar.vue
+++ crust/src/components/AppBar.vue
@@ -3,6 +3,9 @@
     <v-app-bar-title>Indeavor Employee Management</v-app-bar-title>
 
     <v-spacer></v-spacer>
+    <v-btn v-if="!isHome" icon>
+      <v-icon icon="mdi-home" @click="$router.push('/')" />
+    </v-btn>
     <v-btn icon>
       <v-icon icon="mdi-logout-variant" @click="() => auth.logout()" />
     </v-btn>
@@ -15,6 +18,11 @@ export default {
   setup: () => {
     const auth = useAuthStore();
     return { auth };
+  },
+  computed: {
+    isHome() {
+      return this.$route.path === "/";
+    },
   },
 };
 </script>
